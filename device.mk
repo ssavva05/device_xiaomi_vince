bd5dcc7a8eeffbd9431991915f07266d5ee6b966
Дмитрий <waiser-7@mail.ru>
2019-01-18 09:58:58 +0500

    vince: Pre-opt SystemUI
    SystemUI is getting deoptimized, possibly resulting in jank
    (not yet validated); pre-optimize to avoid deoptimization for now.
    Bug: 62832717
    Bug: 35872915
    Test: boot, check for vdex, run for a bit

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI	
+	
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
